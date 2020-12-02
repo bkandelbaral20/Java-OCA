c708c6c86b65bb4e9b181b44e3f83511fe095ad5
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-12-01 22:53:57 -0600

    testing objects

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,17 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        m(1);
     }
+
+    private static void m(Object i) {
+        System.out.println("Object");
+    }
+
+    private static void m(Number i) {
+        System.out.println("Number");
+    }
+    private static void m(Double i) {
+        System.out.println("Double");
+    }
+
 }
