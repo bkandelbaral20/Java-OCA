4dbff1d6cbcb681f531f1b8c5266e8a46df4ec95
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-12-09 19:03:47 -0600

    objects

--- src/OCA/Car.java
+++ src/OCA/Car.java
@@ -1,19 +1,17 @@
 package OCA;
 
-import javax.swing.*;
 
 public class Car {
-    private  String color;
-   private  String type;
+    private String color;
+    private String type;
 
 
     public static void main(String[] args) {
 
 //   Instantiating object
-        Car myCar = new Car();
-        myCar.color ="black";
-        myCar.type ="DeLorean DMC-12";
-
-        System.out.println("My car is in " + myCar.color + " color and " + myCar.type + " type" );
+//        myCar.color = "black";
+//        myCar.type = "DeLorean DMC-12";
+//
+//        System.out.println("My car is in " + myCar.color + " color and " + myCar.type + " type");
     }
 }
---
+++ src/OCA/MyDate.java
@@ -0,0 +1,31 @@
+package OCA;
+
+import java.util.Date;
+
+public class MyDate {
+    int day;
+    int months;
+    int year;
+
+//    constructors
+    public MyDate(int day, int months, int year) {
+        this.day = day;
+        this.months = months;
+        this.year = year;
+    }
+
+    //    no-arg constructor
+    public MyDate() {}
+
+    public String toString(int day, int months, int year){
+        return "11/04/2020";
+    }
+
+    public static void main(String[] args) {
+        MyDate date = new MyDate();
+        System.out.println(date.toString(11,04,2020));
+
+    }
+
+
+}
