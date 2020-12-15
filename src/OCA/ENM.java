254f5876097907f40b646ec136d4447a0acd874e
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-12-14 21:59:44 -0600

    checking days

--- src/OCA/ENM.java
+++ src/OCA/ENM.java
@@ -2,25 +2,32 @@ package OCA;
 
 /*
 we cannot use string so we are using Enumeration(ENM)
-creatings Days object and calling each enum from Enm.java class file
+creating Days object and calling each enum from Enm.java class file
  */
+
 public class ENM {
     public static void main(String[] args) {
 
-         Days today =  Days.FRIDAY;
-         String response = "";
+        Days today = Days.FRIDAY;
 
-         switch(today){
-             case MONDAY:
-                 response ="Get to work!";
-                 break;
-             case FRIDAY:
-                 response ="Time to relax";
-                 break;
-             default:
-                 response = "Practice Java! ";
+//         we can convert days enm to string
+        String theDay = Days.FRIDAY.name();
 
-         }
+//        Checking the days typed by user
+        Days userDay = Days.valueOf("MONDAY");
+        String response = "";
+
+        switch (userDay) {
+            case MONDAY:
+                response = "Get to work!";
+                break;
+            case FRIDAY:
+                response = "Time to relax";
+                break;
+            default:
+                response = "Practice Java! ";
+
+        }
         System.out.println(response);
     }
 }
